e8a41a4062d47027ae8998410071998a7918f7b4
JongIn Yoon <bellpp93@gmail.com>
2022-01-20 00:03:36 +0900

    0119_Home

--- src/실습문제2회/ThreeSixNineGame.java
+++ src/실습문제2회/ThreeSixNineGame.java
@@ -7,7 +7,7 @@ public class ThreeSixNineGame {
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
 		int num;
-		System.out.println("1~99 ÁßÀÇ Á¤¼ö ÇÏ³ª¸¦ ÀÔ·Â");
+		System.out.println("1~9999 ÁßÀÇ Á¤¼ö ÇÏ³ª¸¦ ÀÔ·Â");
 		num = scan.nextInt();
 		if (num == 3 || num == 6 || num == 9) {
 			System.out.println("¹Ú¼öÂ¦");
@@ -16,21 +16,20 @@ public class ThreeSixNineGame {
 				if (num / 10 == 3 || num / 10 == 6 || num / 10 == 9)
 					System.out.println("¹Ú¼öÂ¦Â¦");
 				else
-					System.out.println("¹Ú¼öÂ¦Â¦Â¦");
+					System.out.println("¹Ú¼öÂ¦");
 			} else if (num / 10 == 3 || num / 10 == 6 || num / 10 == 9)
 				System.out.println("¹Ú¼öÂ¦");
 		}
-
-		/*
-        for (int i = 1; i <= 9999; i++) {
-			if ((i % 10) == 3 || (i % 10) == 6 || (i % 10) == 9) {
-				if (i / 10 == 3 || i / 10 == 6 || i / 10 == 9)
-					System.out.println(i + " ¹Ú¼öÂ¦Â¦");// ÀÚ¸´¼ö ¸ðµÎ
-				else
-					System.out.println(i + " ¹Ú¼öÂ¦");// 1ÀÇ ÀÚ¸´¼ö¸¸
-			} else if (i / 10 == 3 || i / 10 == 6 || i / 10 == 9)
-				System.out.println(i + " ¹Ú¼öÂ¦");// 10ÀÇ ÀÚ¸´¼ö¸¸
-		}
-		 */
 	}
 }
+/*
+for (int i = 1; i <= 9999; i++) {
+	if ((i % 10) == 3 || (i % 10) == 6 || (i % 10) == 9) {
+		if (i / 10 == 3 || i / 10 == 6 || i / 10 == 9)
+			System.out.println(i + " ¹Ú¼öÂ¦Â¦");// ÀÚ¸´¼ö ¸ðµÎ
+		else
+			System.out.println(i + " ¹Ú¼öÂ¦");// 1ÀÇ ÀÚ¸´¼ö¸¸
+	} else if (i / 10 == 3 || i / 10 == 6 || i / 10 == 9)
+		System.out.println(i + " ¹Ú¼öÂ¦");// 10ÀÇ ÀÚ¸´¼ö¸¸
+}
+*/
