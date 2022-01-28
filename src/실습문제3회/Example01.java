ccca3211ce083b65ed3a71c6d8cd6f3d6e2cbea1
JongIn Yoon <bellpp93@gmail.com>
2022-01-28 14:35:04 +0900

    0128_문제풀이

--- src/실습문제3회/Example01.java
+++ src/실습문제3회/Example01.java
@@ -5,11 +5,7 @@ class MyClass {
 }
 public class Example01 {
 	public static void main(String[] args) {
-		try {  // 레퍼런스 변수가 null인 상태에서 객체를 참조할 경우 예외처리를 해준다.
-			MyClass a = null;  // 지역변수는 기본값으로 초기화해야 하기 때문에 null값을 준다.
-			a.i = 10;
-		} catch (Exception e) {
-			System.out.println(e);
-		}
+		MyClass a = new MyClass();  //객체를 생성해 줘야 함.
+		a.i = 10;
 	}
 }
--- src/실습문제3회/Example02.java
+++ src/실습문제3회/Example02.java
@@ -1,14 +1,35 @@
 package 실습문제3회;
 
 class Person {
-	public String name;  //private은 아무나 접근 못하기 때문에 모두 접근할 수 있는 public으로 바꾸어준다. 
-	public int age;
+//	public String name;  //private은 아무나 접근 못하기 때문에 모두 접근할 수 있는 public으로 바꾸어준다. 객체 지향 프로그래밍에 알맞지 않음.
+//	public int age;
+	
+	//멤버변수는 가능한 private으로 설정
+	//객체지향 프로그래밍에 적합한 방법은 getter setter 이용
+	private String name;
+	private int age;
+	
+	//멤버변수 하나당 getter, setter 메소드를 한쌍씩 구현한다.
+	/*
+	 * setter 메소드 구현 규칙 => 반드시 매개변수를 받아야하고, return 타입이 void이다.
+	 * getter 메소드 구현 규칙 => 매개변수를 받지 않고, { } 블록 안에 반드시 return이 있어야 한다.
+	 */
+	public String getName() { return name; }
+	public void setName(String name) { this.name = name; }
+	
+	public int getAge() { return age; }
+	public void setAge(int age) { this.age = age; }
 }
 public class Example02 {
 	public static void main(String[] args) {
 		Person aPerson = new Person();
-		aPerson.name = "홍길동";
-		aPerson.age = 26;
-		System.out.println("이름은 " + aPerson.name + " " + "나이는 " + aPerson.age + "입니다.");
+//		aPerson.name = "홍길동";  // public 이용할때 풀이
+//		aPerson.age = 26;
+		
+		aPerson.setName("홍길동");
+		aPerson.setAge(26);
+		
+		System.out.println("이름: " + aPerson.getName());
+		System.out.println("나이: " + aPerson.getAge());
 	}
 }
